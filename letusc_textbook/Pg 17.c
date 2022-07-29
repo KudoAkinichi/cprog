ac1ec070cb4250c81f252a13be7bf979e7bc3570
KudoAkinichi <arookrishna@gmail.com>
2022-07-29 21:30:06 +0530

    Done

---
+++ letusc_textbook/Pg 17.c
@@ -0,0 +1,12 @@
+/*Calculation of aggregate & percentage marks*/
+#include<stdio.h>
+int main()
+{
+    int m1,m2,m3,m4,m5,aggr;
+    float per;
+    printf("\nEnter marks in 5 subjects: ");
+    aggr= m1+m2+m3+m4+m5;
+    per= aggr/5;
+    printf("Aggregate marks= %d\n,aggr");
+    printf("Percentage marks= %f\n, per");
+}
