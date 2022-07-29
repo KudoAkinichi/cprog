79a90047b6b9300e651a85b0d0013f0642e27e38
KudoAkinichi <arookrishna@gmail.com>
2022-07-30 01:13:33 +0530

    ok

--- letusc_textbook/Pg17.c
+++ letusc_textbook/Pg17.c
@@ -6,9 +6,9 @@ int main()
     float per;
     printf("\nEnter marks in 5 subjects: ");
     scanf("%d %d %d %d %d",&m1,&m2,&m3,&m4,&m5);
-    aggr= m1+m2+m3+m4+m5;
+    aggr= m1 + m2 + m3 + m4 + m5;
     per= aggr/5;
-    printf("Aggregate marks= %d\n,aggr");
-    printf("Percentage marks= %f\n, per");
+    printf("Aggregate Marks= %d\n,aggr");
+    printf("Percentage Marks= %f\n, per");
     return 0;
 }
