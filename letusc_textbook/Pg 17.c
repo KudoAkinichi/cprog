7f066beb4d96168516c8336c986ea7ea720993c3
KudoAkinichi <arookrishna@gmail.com>
2022-07-30 00:54:46 +0530

    .

--- letusc_textbook/Pg 17.c
+++ letusc_textbook/Pg 17.c
@@ -5,6 +5,7 @@ int main()
     int m1,m2,m3,m4,m5,aggr;
     float per;
     printf("\nEnter marks in 5 subjects: ");
+    scanf("%d %d %d %d %d",&m1,&m2,&m3,&m4,&m5);
     aggr= m1+m2+m3+m4+m5;
     per= aggr/5;
     printf("Aggregate marks= %d\n,aggr");
