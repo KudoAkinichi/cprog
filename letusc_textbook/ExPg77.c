bfd75e4a9a9f1fc5afc10612e06152431fc3d26c
KudoAkinichi <arookrishna@gmail.com>
2022-08-03 22:56:13 +0530

    SI with while loop let us C pg.77

---
+++ letusc_textbook/ExPg77.c
@@ -0,0 +1,17 @@
+/*Caluclation of simple interest for 3 sets of p,n and r using while loop*/
+#include<stdio.h>
+int main()
+{
+    int p,n,count;
+    float r,si;
+    count=1;
+    while(count<=3)
+    {
+        printf("\nEnter values of p,n and r");
+        scanf("%d%d%f",&p,&n,&r);
+        si=p*n*r/100;
+        printf("Simple interest=\n Rs.%f",si);
+        count=count+1;
+    }
+    return 0;
+}
