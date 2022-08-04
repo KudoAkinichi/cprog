9f6840af12ed80480afc2ed3b7d3a2b341ebbc86
KudoAkinichi <arookrishna@gmail.com>
2022-08-04 20:36:01 +0530

    Cn.5 Prob 5.1 let us c , yay!

---
+++ letusc_textbook/Pg83Prob1.c
@@ -0,0 +1,20 @@
+/*Determine overtime pay of 10 employees*/
+#include<stdio.h>
+int main()
+{
+    float otpay;
+    int hour,i=1;
+    while(i<=10) /*Loop for 10 employees*/
+    {
+        printf("\nEnter no. of hours worked:");
+        scanf("%d",&hour);
+        if(hour>=40)
+            otpay=(hour-40)*300;
+        else
+            otpay=0;
+        printf("Hours=%d\nOvertime pay=Rs.%f\n",hour,otpay);
+        i++;
+    
+    }
+    return 0;
+}
