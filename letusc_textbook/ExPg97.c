383ab069941440d30b363a57f419f3b287398a9c
KudoAkinichi <arookrishna@gmail.com>
2022-08-05 14:05:24 +0530

    To check whether a number is prime or not using while loop

---
+++ letusc_textbook/ExPg97.c
@@ -0,0 +1,23 @@
+/*To check whether a number is prime or not by dividing it sucessively from 2 to a number less then itself*/
+#include<stdio.h>
+int main()
+{
+    int num,i;
+    printf("Enter a number:");
+    scanf("%d",&num);
+    i=2;
+    while(i<=num-1)
+    {
+        if(num%i==0)
+        {
+            printf("Not a prime number\n");
+            break;
+    
+    
+        }
+        i++;
+    }
+    if(i==num)
+        printf("Prime Number\n");
+    return 0;    
+}
